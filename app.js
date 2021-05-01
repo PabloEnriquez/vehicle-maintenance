89d5e769b42843d1cb20c8d328bdfec2985df839
Pablo <pablo.enriquez@mymoons.mx>
2021-05-01 17:10:51 -0500

    add cors validation

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(express.urlencoded({
   extended: true
 }))
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH')
+  next()
+})
+
 app.use(routes)
 
 app.use((req, res, next) => {
